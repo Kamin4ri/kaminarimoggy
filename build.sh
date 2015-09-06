4455c026f2e6de5c7081e8ac278867252a59138e
KingKaminari <ialbuquerqueb792@gmail.com>
2015-09-06 20:50:12 -0300

    build.sh: build for falcon by default

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
-echo -e "Making moto g 4g (peregrine) zImage\n"
-export PATH=$PATH:/opt/toolchain/bin/
+echo -e "Making moto g 3g (falcon) zImage\n"
+export PATH=$HOME/toolchains/Linaro4.9.3-CortexA7-arm-eabi/bin:$PATH
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=arm-cortex_a7-linux-gnueabihf-
 
-make peregrine_defconfig
+make falcon_defconfig
 
-make -j7
+make -j4
+
+# make directories if they don't exist
+if [ ! -d ../zip ] ; then mkdir ../zip; fi;
+if [ ! -d ../zip/kernel ] ; then mkdir ../zip/kernel; fi;
+if [ ! -d ../zip/system ] ; then mkdir ../zip/system; fi;
+if [ ! -d ../zip/system/lib ] ; then mkdir ../zip/system/lib; fi;
+if [ ! -d ../zip/system/lib/modules ] ; then mkdir ../zip/system/lib/modules; fi;
+if [ ! -d ../zip/system/lib/modules/pronto ] ; then mkdir ../zip/system/lib/modules/pronto; fi;
 
 # modules
 find ./ -type f -name '*.ko' -exec cp -f {} ../zip/system/lib/modules/ \;
@@ -16,5 +24,5 @@ mv ../zip/system/lib/modules/wlan.ko ../zip/system/lib/modules/pronto/pronto_wla
 cp -f arch/arm/boot/zImage-dtb ../zip/kernel/
 ls -l ../zip/kernel/zImage-dtb
 cd ../zip
-zip -r -9 moggy_vN.zip * > /dev/null
-mv moggy_vN.zip ../
+zip -r -9 Kaminari-vN.zip * > /dev/null
+mv Kaminari-vN.zip ../
